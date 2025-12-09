7d9a60633e2f401c5fca21e3b3558a4b609d90d8
Eatoes <development@eatoes.com>
2025-12-09 15:57:21 +0530

    added details

---
+++ config.js
@@ -0,0 +1,14 @@
+window.SITE_CONFIG = {
+  title: 'Sunday Soul Sante Hyderabad',
+  tradeName: 'Sunday Soul Sante Hyderabad',
+  legalName: 'MEREDDY VENTURES',
+  phoneNumber: '9063363755',
+  gstNumber: '36ABEFM2637A1ZO',
+  address: 'Plot No. 226, Road No. 78, Phase-III, Jubilee Hills, Hyderabad, Telangana, 500096',
+  openingHours: {
+    monFri: 'MON - FRI: 12PM - 10PM',
+    saturday: 'SATURDAY: 12PM - 12AM',
+    sunday: 'SUNDAY: 12PM - 11PM'
+  },
+  facebookDomainVerification: 'k6uqxp4clzplq9p8njvfa6cw6fuitb'
+};
